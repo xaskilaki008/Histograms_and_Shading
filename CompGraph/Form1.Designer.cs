a808a829626ed79140e5719a9ba9d66c2264db0d
Max <14.01.12@mail.ru>
2020-12-14 21:21:59 +0500

    first commit

---
+++ CompGraph/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace CompGraph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(188, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(577, 281);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Гистограммы";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(60, 350);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(350, 300);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Location = new System.Drawing.Point(470, 350);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(350, 300);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 32);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "+ Яркость";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(164, 32);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "- Яркость";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 126);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(164, 32);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Заливка";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(467, 334);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Гистограмма цвета";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 334);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Гистограмма яркости";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(939, 698);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Комп. графика";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ CompGraph/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CompGraph
+{
+    public partial class Form1 : Form
+    {
+        Graphics graphColored, graphMainImage;
+        Bitmap bitmap;
+
+        // Выбранный цвет
+        Color currentColor;
+        // Выбранный регион
+        Point currentRegion;
+        bool isPaint = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bitmap = new Bitmap(pictureBox1.Image);
+            graphMainImage = Graphics.FromImage(bitmap);
+            currentColor = Color.Black;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Построить гистограмму
+            isPaint = false;
+            int[] brightly = new int[bitmap.Width * bitmap.Height];
+            Bitmap bipmapGist = new Bitmap(256, bitmap.Width * bitmap.Height);
+            Graphics graphBrightly = Graphics.FromImage(bipmapGist);
+            pictureBox2.Image = bipmapGist;
+            Dictionary<int, List<int>> dictRed = new Dictionary<int, List<int>>();
+            Dictionary<int, List<int>> dictGreen = new Dictionary<int, List<int>>();
+            Dictionary<int, List<int>> dictBlue = new Dictionary<int, List<int>>();
+
+            Pen pen = new Pen(Color.Black);
+            int x = 0, y = 0;
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                List<int> red = new List<int>();
+                List<int> green = new List<int>();
+                List<int> blue = new List<int>();
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    int r = Convert.ToInt32(bitmap.GetPixel(i, j).R);
+                    int g = Convert.ToInt32(bitmap.GetPixel(i, j).G);
+                    int b = Convert.ToInt32(bitmap.GetPixel(i, j).B);
+                    red.Add(r);
+                    green.Add(g);
+                    blue.Add(b);
+
+                    brightly[y] = (int)(0.34 * r + 0.5 * g + 0.16 * b); // получили яркость
+                    graphBrightly.DrawLine(pen, new Point(x, 0), new Point(x, brightly[y]));
+                    y++;
+
+                }
+                dictRed.Add(x, red);
+                dictBlue.Add(x, blue);
+                dictGreen.Add(x, green);
+                x++;
+            }
+            Bitmap gist1 = new Bitmap(256, bitmap.Width * bitmap.Height);
+            graphColored = Graphics.FromImage(gist1);
+            pictureBox3.Image = gist1;
+            Drawing(dictRed, new Pen(Color.Red));
+            Drawing(dictGreen, new Pen(Color.Green));
+            Drawing(dictBlue, new Pen(Color.Blue));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // повысить яркость +
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    int r = AddRight(Convert.ToInt32(bitmap.GetPixel(i, j).R));
+                    int g = AddRight(Convert.ToInt32(bitmap.GetPixel(i, j).G));
+                    int b = AddRight(Convert.ToInt32(bitmap.GetPixel(i, j).B));
+                    Color color = Color.FromArgb(r, g, b);
+                    bitmap.SetPixel(i, j, color);
+                }
+            }
+
+            graphMainImage = Graphics.FromImage(bitmap);
+            pictureBox1.Image = bitmap;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // снизить яркость -
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    int r = MinusRight(Convert.ToInt32(bitmap.GetPixel(i, j).R));
+                    int g = MinusRight(Convert.ToInt32(bitmap.GetPixel(i, j).G));
+                    int b = MinusRight(Convert.ToInt32(bitmap.GetPixel(i, j).B));
+                    Color color = Color.FromArgb(r, g, b);
+                    bitmap.SetPixel(i, j, color);
+                }
+            }
+
+            graphMainImage = Graphics.FromImage(bitmap);
+            pictureBox1.Image = bitmap;
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // закрасить область
+            DialogResult dialog = colorDialog1.ShowDialog();
+            if (dialog == DialogResult.OK)
+            {
+                currentColor = colorDialog1.Color;
+                isPaint = true;
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // выбор области
+            if (isPaint)
+            {
+                currentRegion = e.Location;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (isPaint)
+            {
+                PainRegion();
+                pictureBox1.Image = bitmap;
+                graphMainImage = Graphics.FromImage(bitmap);
+            }
+        }
+
+        /// <summary>
+        /// Получить цвет пикселя
+        /// </summary>
+        private Color GetColor(int x, int y)
+        {
+            int r = Convert.ToInt32(bitmap.GetPixel(x, y).R);
+            int g = Convert.ToInt32(bitmap.GetPixel(x, y).G);
+            int b = Convert.ToInt32(bitmap.GetPixel(x, y).B);
+            Color color = Color.FromArgb(r, g, b);
+
+            return color;
+        }
+
+        /// <summary>
+        /// Проверка вышения яркости
+        /// </summary>
+        private int AddRight(int color)
+        {
+            if (color == 255)
+                return 255;
+
+            int newColor = color + 10;
+            if (newColor > 255)
+                return 255;
+
+            return newColor;
+        }
+
+        /// <summary>
+        /// Проверка вычитания яркости
+        /// </summary>
+        private int MinusRight(int color)
+        {
+            if (color == 0)
+                return 0;
+            int newColor = color - 10;
+            if (newColor < 0)
+                return 0;
+
+            return newColor;
+        }
+
+        /// <summary>
+        /// Закрасить регион с одинаковым цветом в выбранный цвет
+        /// </summary>
+        private void PainRegion()
+        {
+            Color color = GetColor(currentRegion.X, currentRegion.Y);
+
+            Color currentColorRegion = color;
+            Stack<Point> points = new Stack<Point>();
+            points.Push(currentRegion);
+            Point currentPoint;
+
+            while (points.Count != 0)
+            {
+                currentPoint = points.Pop();
+                bitmap.SetPixel(currentPoint.X, currentPoint.Y, currentColor);
+
+                int x, y;
+                // Ищем соседей
+                // Верхний пиксель
+                x = currentPoint.X;
+                y = currentPoint.Y + 1;
+                CheckBorderImage(ref x, ref y);
+                CheckBorder(ref points, color, x, y);
+                // Правый пиксель
+                x = currentPoint.X + 1;
+                y = currentPoint.Y;
+                CheckBorderImage(ref x, ref y);
+                CheckBorder(ref points, color, x, y);
+                // Нижний пиксель
+                x = currentPoint.X;
+                y = currentPoint.Y - 1;
+                CheckBorderImage(ref x, ref y);
+                CheckBorder(ref points, color, x, y);
+                // Левый пиксель
+                x = currentPoint.X - 1;
+                y = currentPoint.Y;
+                CheckBorderImage(ref x, ref y);
+                CheckBorder(ref points, color, x, y);
+            }
+        }
+
+        /// <summary>
+        /// Проверить границы заливки
+        /// </summary>
+        private void CheckBorder(ref Stack<Point> points, Color firstColorRegion, int x, int y)
+        {
+            Color pixel = bitmap.GetPixel(x, y);
+            if (pixel.ToArgb() == firstColorRegion.ToArgb() && pixel.ToArgb() != currentColor.ToArgb())
+            {
+                points.Push(new Point(x, y));
+            }
+        }
+
+        /// <summary>
+        /// Проверить границы картинки
+        /// </summary>
+        private void CheckBorderImage(ref int x, ref int y)
+        {
+            if (x >= bitmap.Width)
+                x = bitmap.Width - 1;
+            else if (x < 0)
+                x = 1;
+            if (y >= bitmap.Height)
+                y = bitmap.Height - 1;
+            else if (y < 0)
+                y = 1;
+        }
+
+        /// <summary>
+        /// Рисуем гистограмму цвета
+        /// </summary>
+        private void Drawing(Dictionary<int, List<int>> colors, Pen pen)
+        {
+            for (int x = 0; x < colors.Keys.Count; x++)
+            {
+                for (int y = 0; y < colors[x].Count; y++)
+                {
+                    int color = colors[x][y];
+                    graphColored.DrawLine(pen, new Point(x, 0), new Point(x, color));
+                }
+            }
+        }
+    }
+}
